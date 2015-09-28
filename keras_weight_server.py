57ae9879e536bae829742292413c0fe8a0990b2d
Johan Mathe <johan.mathe@gmail.com>
2015-09-28 18:29:25 +0700

    update w serfver

--- keras_weight_server.py
+++ keras_weight_server.py
@@ -1,55 +1,66 @@
+import threading
 import keras
 import cPickle as pickle
 import zmq
 import sys
 import numpy
-from keras.layers.core import Dense, Dropout, Activation, Flatten
-from keras.layers.convolutional import Convolution2D, MaxPooling2D
 
-port = "5556"
-if len(sys.argv) > 1:
-    port = sys.argv[1]
-    int(port)
+port = 6000
 
-context = zmq.Context()
-g_socket = context.socket(zmq.REP)
-g_socket.bind("tcp://*:%s" % port)
-print 'server started.'
+average_weights_ready = threading.Event()
+layer_weights = None
+n_workers = 2
+sync_done = threading.Event()
+data_for_worker = [threading.Event() for _ in range(n_workers)]
+worker_layer = [None for _ in range(n_workers)]
 
-ARRAY = None
-ARRAYS_RECEIVED = 0
-alpha = 0.9
+def main_thread():
+    global data_for_worker
+    global average_weights_ready
+    global average_weights
+    global worker_layer
+    global n_workers
+    while True:
+        average_weights_ready.clear()
+        for worker in data_for_worker:
+            worker.wait()
+        average_weights = worker_layer[0]
+        for i, w_layer in enumerate(worker_layer[1:]):
+            for j, layer in enumerate(w_layer):
+                for k, inside_layer in enumerate(layer):
+                    average_weights[j][k] += worker_layer[i][j][k]
+        for j, layer in enumerate(average_weights):
+            for k, inside_layer in enumerate(layer):
+                average_weights[j][k] /= n_workers
+        average_weights_ready.set()
+        print 'one pass'
 
 
-def recv_array(socket, flags=0, copy=True, track=False):
-    """recv a numpy array"""
-    md = socket.recv_json(flags=flags)
-    msg = socket.recv(flags=flags, copy=copy, track=track)
-    buf = buffer(msg)
-    A = numpy.frombuffer(buf, dtype=md['dtype'])
-    return A.reshape(md['shape'])
+def worker(i):
+    global data_for_worker
+    global average_weights_ready
+    global average_weights
+    global worker_layer
+    context = zmq.Context()
+    socket = context.socket(zmq.REP)
+    server_string = "tcp://*:%d" % (port + i)
+    print 'thread %d listening on %s' % (i, server_string)
+    socket.bind(server_string)
+    while True:
+        #  Wait for next request from client
+        data_for_worker[i].clear()
+        data = socket.recv()
+        layers = pickle.loads(data)
+        worker_layer[i] = layers
+        data_for_worker[i].set()
+        average_weights_ready.wait()
+        socket.send(pickle.dumps(average_weights))
 
-
-def send_array(socket, A, flags=0, copy=True, track=False):
-    """send a numpy array with metadata"""
-    md = dict(
-        dtype=str(A.dtype),
-        shape=A.shape,
-    )
-    socket.send_json(md, flags | zmq.SNDMORE)
-    return socket.send(A, flags, copy=copy, track=track)
-
-while True:
-    #  Wait for next request from client
-    new_array = g_socket.recv()
-    print 'new array received'
-    layers = pickle.loads(new_array)
-    for layer in layers:
-        print layer.weights
-    g_socket.send(pickle.dumps(layers))
-    #if ARRAY is None:
-    #    ARRAY = new_array
-    #else:
-    #    print 'computing new array...'
-    #    ARRAY = alpha * ARRAY + (1-alpha) * new_array
-    #ARRAYS_RECEIVED += 1
+threads = []
+for i in range(n_workers):
+    t = threading.Thread(target=worker, args=(i,))
+    threads.append(t)
+    t.start()
+main_t = threading.Thread(target=main_thread)
+main_t.start()
+[t.join() for t in threads]
