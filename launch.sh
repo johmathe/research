6727f9fbc9f222bf7af2c5852e8d5e86efa465df
Johan Mathe <johan.mathe@gmail.com>
2015-10-01 15:04:15 +0700

    update launch

--- launch.sh
+++ launch.sh
@@ -2,5 +2,5 @@
 killall python
 sleep 1
 python keras_weight_server.py &
-python cifar10_cnn.py 0  &
-python cifar10_cnn.py 1  &
+python cifar10_cnn.py 0 localhost > /dev/null  &
+python cifar10_cnn.py 1 localhost &
